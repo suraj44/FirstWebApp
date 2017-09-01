e97719197e39f6549eadd9e6485a2eaea63a4111
Suraj <suraj1998@gmail.com>
2017-09-01 15:16:15 +0530

    Implemented each blog post as an object that's part of a list in Django

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.utils import timezone
+from .models import Post
 def post_list(request):
-	return render(request, 'blog/post_list.html',{})
+	#posts is the name of the query set
+	posts = Post.objects.filter(published_data__lte = timezone.now()).order_by('published_data');
+	return render(request, 'blog/post_list.html',{'posts':posts})
